0a729957b9cd90999e933a45d44189e0e9a1f944
Christopher Groskopf <staringmonkey@gmail.com>
2011-04-02 16:39:28 -0500

    Added first test for xlsx.

---
+++ tests/test_convert/test_xlsx.py
@@ -0,0 +1,12 @@
+import unittest
+
+from csvkit.convert import xlsx
+
+class TestXLSX(unittest.TestCase):
+    def test_xlsx(self):
+        with open('examples/test.xlsx', 'r') as f:
+            output = xlsx.xlsx2csv(f)
+        
+        with open('examples/testxlsx_converted.csv', 'r') as f:
+            self.assertEquals(f.read(), output)
+
