a9ba11b5c324b5f7b79eba553e62f5d09574942d
cgroskopf <cgroskopf@tribune.com>
2011-04-04 12:59:49 -0500

    xlsx import implemented but broken, due to apparent date-parsing bug in openpyxl?

--- csvkit/convert/xlsx.py
+++ csvkit/convert/xlsx.py
@@ -7,114 +7,112 @@ from openpyxl.cell import Cell
 
 import utils
 
-"""
-TYPE_STRING = 's'
-TYPE_FORMULA = 'f'
-TYPE_NUMERIC = 'n'
-TYPE_BOOL = 'b'
-TYPE_NULL = 's'
-TYPE_INLINE = 'inlineStr'
-TYPE_ERROR = 'e'
-"""
-
-class XLSXDataError(Exception):
-    """
-    Exception raised when there is a problem converting XLSX data.
-    """
-    def __init__(self, msg):
-        self.msg = msg
-
-def normalize_strings(values):
-    """
-    Normalize a column of text cells.
-    """
-    return [v if v else None for v in values]
-
-def normalize_numerics(values):
-    """
-    Normalize a column of numeric cells.
-    """
-    # Test if all values are whole numbers, if so coerce floats it ints
-    integral = True
-
-    for v in values:
-        if v and v % 1 != 0:
-            integral = False
-            break
-
-    if integral:
-        return [int(v) if v not in ('', None) else None for v in values]
-    else:
-        # Convert blanks to None
-        return [v if v else None for v in values]
-
-def normalize_booleans(values):
-    """
-    Normalize a column of boolean cells.
-    """
-    return [bool(v) if v != '' else None for v in values] 
-
-def normalize_nulls(values):
-    """
-    Normalize a column which contains only empty cells.
-    """
-    return [None] * len(values)
-
-def normalize_dates(values):
-    """
-    Normalize a column of date cells.
-    """
-    normal_values = []
-    normal_types_set = set()
-
-    for v in values:
-        if v == None or v == '':
-            normal_values.append(None)
-            continue
-
-        normal_values.append(v)
-        normal_types_set.add(type(v))
-
-    if len(normal_types_set) == 1:
-        # No special handling if column contains only one type
-        pass 
-    elif normal_types_set == set([datetime.datetime, datetime.date]):
-        # If a mix of dates and datetimes, up-convert dates to datetimes
-        for i, v in enumerate(normal_values):
-            if v.__class__ == datetime.date:
-                normal_values[i] = datetime.datetime.combine(v, datetime.time())
-    elif normal_types_set == set([datetime.datetime, datetime.time]):
-        # Datetimes and times don't mix
-        raise XLSXDataError('Column contains a mix of times and datetimes (this is not supported).')
-    elif normal_types_set == set([datetime.date, datetime.time]):
-        # Dates and times don't mix
-        raise XLSXDataError('Column contains a mix of dates and times (this is not supported).')
-
-    # Natural serialization of dates and times by csv.writer is insufficent so they get converted back to strings at this point
-    return [v.isoformat() if v != None else None for v in normal_values] 
-
-NORMALIZERS = {
-    Cell.TYPE_STRING: normalize_strings,
-    Cell.TYPE_NUMERIC: normalize_numerics,
-    Cell.TYPE_BOOL: normalize_booleans,
-    Cell.TYPE_NULL: normalize_nulls,
-    'DATE': normalize_dates,
-}
-
-def determine_column_type(types):
+def normalize_column_type(l):
     """
     Determine the correct type for a column from a list of cell types.
+
+    NOTE: Excel/openpyxl datatypes are worthless for this purpose, so we do type inference ourselves.
     """
-    types_set = set(types)
-    types_set.discard(Cell.TYPE_NULL)
-
-    if len(types_set) > 1:
-        raise XLSXDataError('Column contains multiple data types: %s' % str(types_set))
-
+    # Are they null?
     try:
-        return types_set.pop()
-    except KeyError:
-        return Cell.TYPE_NULL
+        for x in l:
+            if x != '' and x != None:
+                raise ValueError('Not null')
+
+        return None, [None] * len(l)
+    except ValueError:
+        pass
+
+    # Are they boolean?
+    try:
+        normal_values = []
+
+        for x in l:
+            if x in ['', None]:
+                normal_values.append(None)
+            elif str(x).lower() in ('1', 'yes', 'true'):
+                normal_values.append(True)
+            elif str(x).lower() in ('0', 'no', 'false'):
+                normal_values.append(False)
+            else:
+                raise ValueError('Not boolean')
+
+        return bool, normal_values
+    except ValueError:
+        pass
+
+    # Are they integers?
+    try:
+        normal_values = []
+
+        for x in l:
+            if x in ['', None]:
+                normal_values.append(None)
+                continue
+
+            if type(x) in [int, float] and x % 1 == 0:
+                normal_values.append(int(x))
+                continue
+
+            raise ValueError('Not int')
+            
+        return int, normal_values 
+    except ValueError:
+        pass
+
+    # Are they floats?
+    try:
+        normal_values = []
+
+        for x in l:
+            if x in ['', None]:
+                normal_values.append(None)
+                continue
+
+            if type(x) in [int, float]:
+                normal_values.append(float(x))
+                continue
+
+            raise ValueError('Not int')
+            
+        return float, normal_values 
+    except ValueError:
+        pass
+
+    # Are they datetimes?
+    try:
+        normal_values = []
+        normal_types_set = set()
+
+        for v in l:
+            if v in ['', None]:
+                normal_values.append(None)
+                continue
+
+            normal_values.append(v)
+            normal_types_set.add(type(v))
+
+        if len(normal_types_set) == 1:
+            # No special handling if column contains only one type
+            pass 
+        elif normal_types_set == set([datetime.datetime, datetime.date]):
+            # If a mix of dates and datetimes, up-convert dates to datetimes
+            for i, v in enumerate(normal_values):
+                if v.__class__ == datetime.date:
+                    normal_values[i] = datetime.datetime.combine(v, datetime.time())
+        elif normal_types_set == set([datetime.datetime, datetime.time]):
+            # Datetimes and times don't mix
+            raise ValueError('Column contains a mix of times and datetimes (this is not supported).')
+        elif normal_types_set == set([datetime.date, datetime.time]):
+            # Dates and times don't mix
+            raise ValueError('Column contains a mix of dates and times (this is not supported).')
+            
+        return normal_types_set.pop(), normal_values 
+    except ValueError:
+        pass
+
+    # Don't know what they are, so they must just be strings 
+    return str, [x if x not in ['', None] else None for x in l]    
 
 def xlsx2csv(f):
     """
@@ -126,17 +124,11 @@ def xlsx2csv(f):
     data_columns = []
 
     for column in sheet.columns:
-        column_name = column[0]
+        column_name = column[0].value
         values = [i.value for i in column[1:]]
-        types = [i.data_type if not i.is_date() else 'DATE' for i in column[1:]]
 
         try:
-            column_type = determine_column_type(types)
-
-            if column_type in [Cell.TYPE_FORMULA, Cell.TYPE_INLINE, Cell.TYPE_ERROR]:
-                raise XLSXDataError('Column contains unsupported data type "%s"' % column_type)
-             
-            normal_values = NORMALIZERS[column_type](values)
+            t, normal_values = normalize_column_type(values)
         except XLSXDataError, e:
             e.msg = 'Error in column %i, "%s": %s' % (i, column_name, e.msg)
             raise e
--- tests/test_convert/test_xlsx.py
+++ tests/test_convert/test_xlsx.py
@@ -5,46 +5,46 @@ from csvkit.convert import xlsx
 
 class TestXLSX(unittest.TestCase):
     def test_normalize_nulls(self):
-        normal_values = xlsx.normalize_nulls(['', '', ''])
-        self.assertEquals(normal_values, [None, None, None])
+        normal_values = xlsx.normalize_column_type(['', '', ''])
+        self.assertEquals(normal_values, (None, [None, None, None]))
 
     def test_normalize_strings(self):
-        normal_values = xlsx.normalize_strings(['This', '', 'text'])
-        self.assertEquals(normal_values, ['This', None, 'text'])
+        normal_values = xlsx.normalize_column_type(['This', '', 'text'])
+        self.assertEquals(normal_values, (str, ['This', None, 'text']))
 
     def test_normalize_integers(self):
-        normal_values = xlsx.normalize_numerics([1.0, 418000000, -817, 0.0, ''])
-        self.assertEquals(normal_values, [1, 418000000, -817, 0, None])
+        normal_values = xlsx.normalize_column_type([1.0, 418000000, -817, 0.0, ''])
+        self.assertEquals(normal_values, (int, [1, 418000000, -817, 0, None]))
 
     def test_normalize_floats(self):
-        normal_values = xlsx.normalize_numerics([1.01, 418000000, -817.043, 0.0001, ''])
-        self.assertEquals(normal_values, [1.01, 418000000.0, -817.043, 0.0001, None])
+        normal_values = xlsx.normalize_column_type([1.01, 418000000, -817.043, 0.0001, ''])
+        self.assertEquals(normal_values, (float, [1.01, 418000000.0, -817.043, 0.0001, None]))
     
     def test_normalize_booleans(self):
         # TKTK
         pass
     
     def test_dates_column_dates(self):
-        normal_values = xlsx.normalize_dates([datetime.date(2004, 6, 5), datetime.date(1984, 2, 23), datetime.date(1907, 12, 25), ''])
-        self.assertEquals(normal_values, ['2004-06-05', '1984-02-23', '1907-12-25', None])
+        normal_values = xlsx.normalize_column_type([datetime.date(2004, 6, 5), datetime.date(1984, 2, 23), datetime.date(1907, 12, 25), ''])
+        self.assertEquals(normal_values, (datetime.date, [datetime.date(2004, 6, 5), datetime.date(1984, 2, 23), datetime.date(1907, 12, 25), None]))
     
     def test_dates_column_times(self):
-        normal_values = xlsx.normalize_dates([datetime.time(14, 30, 0), datetime.time(4, 5, 37), datetime.time(0, 0, 0), ''])
-        self.assertEquals(normal_values, ['14:30:00', '04:05:37', '00:00:00', None])
+        normal_values = xlsx.normalize_column_type([datetime.time(14, 30, 0), datetime.time(4, 5, 37), datetime.time(0, 0, 0), ''])
+        self.assertEquals(normal_values, (datetime.time, [datetime.time(14, 30, 0), datetime.time(4, 5, 37), datetime.time(0, 0, 0), None]))
 
     def test_dates_column_datetimes(self):
-        normal_values = xlsx.normalize_dates([datetime.datetime(2004, 6, 5, 14, 30, 23), datetime.datetime(1984, 2, 23, 0, 0, 0), datetime.datetime(1907, 12, 25, 2, 0, 0), ''])
-        self.assertEquals(normal_values, ['2004-06-05T14:30:23', '1984-02-23T00:00:00', '1907-12-25T02:00:00', None])
+        normal_values = xlsx.normalize_column_type([datetime.datetime(2004, 6, 5, 14, 30, 23), datetime.datetime(1984, 2, 23, 0, 0, 0), datetime.datetime(1907, 12, 25, 2, 0, 0), ''])
+        self.assertEquals(normal_values, (datetime.datetime, [datetime.datetime(2004, 6, 5, 14, 30, 23), datetime.datetime(1984, 2, 23, 0, 0, 0), datetime.datetime(1907, 12, 25, 2, 0, 0), None]))
     
     def test_dates_column_dates_and_times(self):
-        self.assertRaises(xlsx.XLSXDataError, xlsx.normalize_dates, [datetime.date(2004, 6, 5), datetime.time(4, 5, 37), ''])
+        self.assertRaises(xlsx.XLSXDataError, xlsx.normalize_column_type, [datetime.date(2004, 6, 5), datetime.time(4, 5, 37), ''])
 
     def tests_dates_column_dates_and_datetimes(self):
-        normal_values = xlsx.normalize_dates([datetime.date(2004, 6, 5), datetime.datetime(2001, 1, 1, 4, 5, 37), ''])
-        self.assertEquals(normal_values, ['2004-06-05T00:00:00', '2001-01-01T04:05:37', None])
+        normal_values = xlsx.normalize_column_type([datetime.date(2004, 6, 5), datetime.datetime(2001, 1, 1, 4, 5, 37), ''])
+        self.assertEquals(normal_values, (datetime.datetime, [datetime.date(2004, 6, 5), datetime.datetime(2001, 1, 1, 4, 5, 37), None]))
 
     def test_dates_column_times_and_datetimes(self):
-        self.assertRaises(xlsx.XLSXDataError, xlsx.normalize_dates, [datetime.datetime(2004, 6, 5, 0, 30, 0), datetime.time(4, 5, 37)])
+        self.assertRaises(xlsx.XLSXDataError, xlsx.normalize_column_type, [datetime.datetime(2004, 6, 5, 0, 30, 0), datetime.time(4, 5, 37)])
     
     def test_xlsx(self):
         with open('examples/test.xlsx', 'rb') as f:
