a9b9d3d922197ccea5410d44e015dbbed283da7c
cgroskopf <cgroskopf@tribune.com>
2011-04-04 16:51:29 -0500

    Working proof-of-concept for csvsql.

--- csvkit/sql.py
+++ csvkit/sql.py
@@ -2,29 +2,37 @@
 
 import datetime
 
-from sqlalchemy import Column, MetaData, Table, Integer
+from sqlalchemy import Column, MetaData, Table
+from sqlalchemy import Boolean, Date, DateTime, Float, Integer, String, Time
 from sqlalchemy.schema import CreateTable
 
 def make_column(column_name, normal_type, normal_column):
+    column_kwargs = {}
+    type_kwargs = {}
+
     for data in normal_column:
         if normal_type == bool:
-            pass
+            column_type = Boolean  
         elif normal_type == int:
-            pass
+            column_type = Integer
         elif normal_type == float:
-            pass
+            column_type = Float
         elif normal_type == datetime.datetime:
-            pass
+            column_type = DateTime
         elif normal_type == datetime.date:
-            pass
+            column_type = Date
         elif normal_type == datetime.time:
-            pass
+            column_type = Time
+        elif normal_type == None:
+            column_type = String
+            type_kwargs['length'] = 32
         elif normal_type == str:
-            pass
+            column_type = String
+            type_kwargs['length'] = max([len(d) if d else 0 for d in normal_column])
+        else:
+            raise ValueError('Unexpected normalized column type: %s' % normal_type)
 
-    t = Integer
-
-    column = Column(column_name, t)
+    column = Column(column_name, column_type(**type_kwargs), **column_kwargs)
 
     return column
 
