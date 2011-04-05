271dc957b295470e74482f575934ecb1f2604b9d
Christopher Groskopf <staringmonkey@gmail.com>
2011-04-04 20:16:50 -0500

    Added dialect switch for csvsql.

--- csvkit/sql.py
+++ csvkit/sql.py
@@ -7,41 +7,49 @@ from sqlalchemy import Boolean, Date, DateTime, Float, Integer, String, Time
 from sqlalchemy.schema import CreateTable
 
 def make_column(column_name, normal_type, normal_column):
-    column_kwargs = {}
+    column_kwargs = {'nullable': False}
     type_kwargs = {}
 
-    for data in normal_column:
-        if normal_type == bool:
-            column_type = Boolean  
-        elif normal_type == int:
-            column_type = Integer
-        elif normal_type == float:
-            column_type = Float
-        elif normal_type == datetime.datetime:
-            column_type = DateTime
-        elif normal_type == datetime.date:
-            column_type = Date
-        elif normal_type == datetime.time:
-            column_type = Time
-        elif normal_type == None:
-            column_type = String
-            type_kwargs['length'] = 32
-        elif normal_type == str:
-            column_type = String
-            type_kwargs['length'] = max([len(d) if d else 0 for d in normal_column])
-        else:
-            raise ValueError('Unexpected normalized column type: %s' % normal_type)
+    if normal_type == bool:
+        column_type = Boolean  
+    elif normal_type == int:
+        column_type = Integer
+    elif normal_type == float:
+        column_type = Float
+    elif normal_type == datetime.datetime:
+        column_type = DateTime
+    elif normal_type == datetime.date:
+        column_type = Date
+    elif normal_type == datetime.time:
+        column_type = Time
+    elif normal_type == None:
+        column_type = String
+        type_kwargs['length'] = 32
+    elif normal_type == str:
+        column_type = String
+        type_kwargs['length'] = max([len(d) if d else 0 for d in normal_column])
+    else:
+        raise ValueError('Unexpected normalized column type: %s' % normal_type)
+
+    for d in normal_column:
+        if d == None:
+            column_kwargs['nullable'] = True
+            break
 
     column = Column(column_name, column_type(**type_kwargs), **column_kwargs)
 
     return column
 
-def make_create_table(column_names, normal_types, normal_columns):
+def make_create_table(column_names, normal_types, normal_columns, dialect=None):
     metadata = MetaData()
     table = Table('csvsql', metadata)
 
     for i in range(len(column_names)):
         c = make_column(column_names[i], normal_types[i], normal_columns[i])
         table.append_column(c)
-    
-    return CreateTable(table).compile()
+
+    if dialect:
+        module = __import__('sqlalchemy.dialects.%s' % dialect, fromlist=['dialect'])
+        dialect = module.dialect() 
+
+    return CreateTable(table).compile(dialect=dialect)
