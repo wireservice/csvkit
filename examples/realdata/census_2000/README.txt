3fcc9b5a447c71162f00c53576ec410733862be1
Joe Germuska <joe@germuska.com>
2011-04-18 22:06:19 -0500

    adjust fixed to allow more flexible specification of column, start, and length

--- examples/realdata/census_2000/README.txt
+++ examples/realdata/census_2000/README.txt
@@ -1,3 +1,6 @@
+GEOGRAPHIC HEADER FILE
+----------------------
+
 The file 'usgeo.upl' in this directory is the "Geographic Header File" distributed with the 
 "Race and Hispanic or Latino Summary File" dataset from the 2000 US Census. 
 
@@ -19,3 +22,27 @@ This will print the NAME column to STDOUT.
 The documentation for that dataset notes, "The geographic header record is standard across all 
 electronic data products from Census 2000."  Therefore, the same schema can also be used to convert 
 the geographic header files for the more in-depth files accompanying the SF1 and other data sets.
+
+VOTING RIGHTS DETERMINATION
+---------------------------
+By law, the Director of the Census is to determine which political subdivisions are subject to the minority 
+language assistance provisions of Section 203 of the Voting Rights Act.
+
+<http://www.census.gov/mp/www/cat/decennial_census_2000/voting_rights_determination_file_on_cd-rom.html>
+
+The file VROUTFSJ.TXt is the result of the report, limited to the subdivisions so identified. 
+<http://www2.census.gov/census_2000/datasets/determination/VROUTFSJ.TXt>
+(This data is actually also made available as CSV, but there is also a complete fixed-width file which might be 
+interesting for analysis outside the specific question of minority language voting assistance which is in the 
+same format as the test file here, but which is much too large to use for an example.)
+
+To convert the fixed-width voting rights determination file to CSV, use:
+
+% in2csv -f fixed -s determination_schema.csv VROUTFSJ.TXt > determination.csv
+
+To see that it worked, try:
+
+% csvcut -c 18 determination.csv
+
+For complete documentation of the meaning of the fields described in determination_schema.csv consult:
+<http://www2.census.gov/census_2000/datasets/determination/vr_doc_rev7.wpd>
--- tests/test_convert/test_fixed.py
+++ tests/test_convert/test_fixed.py
@@ -9,4 +9,53 @@ class TestFixed(unittest.TestCase):
                 output = fixed.fixed2csv(f, schema)
         
         with open('examples/testfixed_converted.csv', 'r') as f:
-            self.assertEquals(f.read(), output)
+            self.assertEqual(f.read(), output)
+
+    def test_row_decoder_init(self):
+        rd = fixed.SchemaRowDecoder(start=1,length=2,column=3)
+        self.assertEqual(1,rd.start)
+        self.assertEqual(2,rd.length)
+        self.assertEqual(3,rd.column)
+        
+        rd = fixed.SchemaRowDecoder(row=['column','start','length'])
+        self.assertEqual(1,rd.start)
+        self.assertEqual(2,rd.length)
+        self.assertEqual(0,rd.column)
+        
+        
+    def test_row_decoder_in_action(self):
+        rd = fixed.SchemaRowDecoder(start=1,length=2,column=3)
+
+        (column, start, length) = rd(['This is a comment','0','1','column_name'])
+        self.assertEqual(False,rd.one_based)
+        self.assertEqual('column_name',column)
+        self.assertEqual(0, start)
+        self.assertEqual(1, length)
+        
+        (column, start, length) = rd(['This is another comment','1','5','column_name2'])
+        self.assertEqual(False,rd.one_based)
+        self.assertEqual('column_name2',column)
+        self.assertEqual(1, start)
+        self.assertEqual(5, length)
+        
+        (column, start, length) = rd(['yet another comment','9','14','column_name3'])
+        self.assertEqual(False,rd.one_based)
+        self.assertEqual('column_name3',column)
+        self.assertEqual(9, start)
+        self.assertEqual(14, length)
+        
+
+    def test_one_based_row_decoder(self):
+        rd = fixed.SchemaRowDecoder(row=['column','start','length'])
+
+        (column, start, length) = rd(['LABEL', '1', '5' ])
+        self.assertEqual(True,rd.one_based)
+        self.assertEqual('LABEL',column)
+        self.assertEqual(0, start)
+        self.assertEqual(5, length)
+
+        (column, start, length) = rd(['LABEL2', '6', '15' ])
+        self.assertEqual('LABEL2',column)
+        self.assertEqual(5, start)
+        self.assertEqual(15, length)
+        
