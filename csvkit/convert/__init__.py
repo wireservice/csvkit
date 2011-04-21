f2af6aa1e7ca6a8facd8c72d620489cd7b9626d5
cgroskopf <cgroskopf@tribune.com>
2011-04-21 14:51:28 -0500

    Eliminated unnecessary convert/utils.py

--- csvkit/convert/__init__.py
+++ csvkit/convert/__init__.py
@@ -28,3 +28,23 @@ def convert(f, format, schema=None, **kwargs):
     else:
         raise ValueError('format "%s" is not supported' % format)
 
+def guess_format(filename):
+    """
+    Try to guess a file's format based on its extension (or lack thereof).
+    """
+    last_period = filename.rfind('.')
+
+    if last_period == -1:
+        # No extension: assume fixed-width
+        return 'fixed'
+
+    extension = filename[last_period + 1:]
+
+    if extension == 'xls':
+        return extension
+    elif extension == 'csv':
+        return extension
+    elif extension == 'fixed':
+        return extension
+
+    return None
