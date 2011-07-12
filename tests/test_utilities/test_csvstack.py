a98ed6144bc6d5c661e597fedb091c4ac16469cd
Bryan Silverthorn <bcs@cargo-cult.org>
2011-07-12 14:53:20 -0500

    Add two basic tests of the csvstack utility.

---
+++ tests/test_utilities/test_csvstack.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import sys
+import StringIO
+import unittest
+
+from csvkit import CSVKitReader
+from csvkit.utilities.stack import CSVStack
+
+class TestCSVStack(unittest.TestCase):
+    def test_explicit_grouping(self):
+        # stack two CSV files
+        args = ["--groups", "asd,sdf", "-n", "foo", "examples/dummy.csv", "examples/dummy2.csv"]
+        output_file = StringIO.StringIO()
+        utility = CSVStack(args, output_file)
+
+        utility.main()
+
+        # verify the stacked file's contents
+        input_file = StringIO.StringIO(output_file.getvalue())
+        reader = CSVKitReader(input_file)
+
+        self.assertEqual(reader.next(), ["foo", "a", "b", "c"])
+        self.assertEqual(reader.next()[0], "asd")
+        self.assertEqual(reader.next()[0], "sdf")
+
+    def test_filenames_grouping(self):
+        # stack two CSV files
+        args = ["--filenames", "-n", "path", "examples/dummy.csv", "examples/dummy2.csv"]
+        output_file = StringIO.StringIO()
+        utility = CSVStack(args, output_file)
+
+        utility.main()
+
+        # verify the stacked file's contents
+        input_file = StringIO.StringIO(output_file.getvalue())
+        reader = CSVKitReader(input_file)
+
+        self.assertEqual(reader.next(), ["foo", "a", "b", "c"])
+        self.assertEqual(reader.next()[0], "asd")
+        self.assertEqual(reader.next()[0], "sdf")
+
