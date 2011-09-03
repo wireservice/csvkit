d832c4d3952f9be40d0d6d34257e2e5b85da1e9e
Christopher Groskopf <staringmonkey@gmail.com>
2011-09-03 15:41:30 -0500

    Migrate csvlook to new organization.

---
+++ csvkit/utilities/csvlook.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+
+from csvkit import CSVKitReader
+from csvkit.cli import CSVKitUtility 
+
+class CSVLook(CSVKitUtility):
+    description = 'Render a CSV file in the console as a fixed-width table.'
+    override_flags = 'l'
+
+    def add_arguments(self):
+        pass
+
+    def main(self):
+        rows = CSVKitReader(self.args.file, **self.reader_kwargs)
+        rows = list(rows)
+
+        widths = []
+
+        for row in rows:
+            for i, v in enumerate(row):
+                try:
+                    if len(v) > widths[i]:
+                        widths[i] = len(v)
+                except IndexError:
+                    widths.append(len(v))
+
+        # Width of the fields, plus space between, plus fore and aft dividers 
+        divider = '-' * (sum(widths) + (3 * len(widths)) + 3)
+
+        self.output_file.write('%s\n' % divider)
+
+        for i, row in enumerate(rows):
+            self.input_line_number = i + 1
+            output = []
+
+            for j, d in enumerate(row):
+                if d is None:
+                    d = ''
+                output.append(' %s ' % unicode(d).ljust(widths[j]))
+
+            self.output_file.write(('| %s |\n' % ('|'.join(output))).encode('utf-8'))
+
+            if i == 0 or i == len(rows) - 1:
+                self.output_file.write('%s\n' % divider)
+
+if __name__ == '__main__':
+    utility = CSVLook()
+    utility.main()
---
+++ tests/test_utilities/test_csvlook.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import StringIO
+import unittest
+
+from csvkit import CSVKitReader
+from csvkit.utilities.csvlook import CSVLook
+
+class TestCSVLook(unittest.TestCase):
+    def test_simple(self):
+        args = ['examples/dummy3.csv']
+        output_file = StringIO.StringIO()
+        utility = CSVLook(args, output_file)
+
+        utility.main()
+
+        input_file = StringIO.StringIO(output_file.getvalue())
+
+        self.assertEqual(input_file.next(), '---------------\n')
+        self.assertEqual(input_file.next(), '|  a | b | c  |\n')
+        self.assertEqual(input_file.next(), '---------------\n')
+        self.assertEqual(input_file.next(), '|  1 | 2 | 3  |\n')
+        self.assertEqual(input_file.next(), '|  1 | 4 | 5  |\n')
+        self.assertEqual(input_file.next(), '---------------\n')
+
