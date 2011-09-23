d1e25f7826c9c3c3bd153c5cba85c94106d6ac45
Alejandro Companioni <achompas@gmail.com>
2011-09-23 17:29:32 -0300

    test_convert_xls() (via github.com)

--- tests/test_utilities/test_in2csv.py
+++ tests/test_utilities/test_in2csv.py
@@ -4,13 +4,35 @@ import unittest
 import StringIO
 
 from csvkit.utilities.in2csv import In2CSV
+from csvkit.utilities.csvstat import CSVStat
 
 class TestIn2CSV(unittest.TestCase):
     def test_bad_format(self):
         args = ['-f', 'foo', 'examples/dummy.csv']
         output_file = StringIO.StringIO()
+        utility = In2CSV(args, output_file)
 
-        self.assertRaises(In2CSV.argparser.error, In2CSV(args, output_file))
+        self.assertRaises(In2CSV.argparser.error, utility.main())
+        
+    def test_convert_xls(self):
+        args = ['-f', 'xls', 'examples/test.xls']
+        output_file = StringIO.StringIO()
+        
+        utility = In2CSV(args, output_file)
+        utility.main()
+        
+        # Add long string here with what the XLS file SHOULD look like,
+        # then compare output_file.getvalue() with the string. Refer to
+        # examples/testxls_converted.csv.
+        target_output = " "
+                
+        self.assertEqual(output_file.getvalue(), target_output)
+    
+    def test_stdin_json(self):
+        """
+        Test whether JSON input into <stdin> converts properly. How can I
+        submit data to <stdin> automatically? stdin.write.out()?
+        """
     
     # Other potential tests:
     # 1. Check file formats for proper conversion -- fixed, xls, json.
