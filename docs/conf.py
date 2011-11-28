93beda0b796c1c43eaa9c2072bc83732e072e61f
Christopher Groskopf <staringmonkey@gmail.com>
2011-11-28 08:35:21 -0600

    Rev to 0.4.0 for release.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, Christopher Groskopf'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.0'
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags.
 release = '0.3.0 (beta)'
 
