66b92a841ff979e2e2f510d1cfd8549d14c91b02
Christopher Groskopf <staringmonkey@gmail.com>
2012-01-11 10:19:26 -0600

    Fix bug in no-constraints.

--- csvkit/utilities/csvsql.py
+++ csvkit/utilities/csvsql.py
@@ -34,9 +34,6 @@ class CSVSQL(CSVKitUtility):
         else:
             self.argparser.error('The --table argument is required when providing data over STDIN.')
 
-        if self.args.no_constraints:
-            no_constraints = True
-
         if self.args.dialect and self.args.connection_string:
             self.argparser.error('The --dialect option is only valid when --db is not specified.')
 
@@ -52,7 +49,7 @@ class CSVSQL(CSVKitUtility):
             except ImportError:
                 raise ImportError('You don\'t appear to have the necessary database backend installed for connection string you\'re trying to use.. Available backends include:\n\nPostgresql:\tpip install psycopg2\nMySQL:\t\tpip install MySQL-python\n\nFor details on connection strings and other backends, please see the SQLAlchemy documentation on dialects at: \n\nhttp://www.sqlalchemy.org/docs/dialects/\n\n')
 
-            sql_table = sql.make_table(csv_table, table_name, no_constraints, metadata)
+            sql_table = sql.make_table(csv_table, table_name, self.args.no_constraints, metadata)
             sql_table.create()
 
             if self.args.insert:
@@ -64,7 +61,7 @@ class CSVSQL(CSVKitUtility):
 
         # Writing to file
         else:
-            sql_table = sql.make_table(csv_table, table_name, no_constraints)
+            sql_table = sql.make_table(csv_table, table_name, self.args.no_constraints)
             self.output_file.write((u'%s\n' % sql.make_create_table_statement(sql_table, dialect=self.args.dialect)).encode('utf-8'))
 
 if __name__ == '__main__':
