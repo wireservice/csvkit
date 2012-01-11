b4b8054c48f9eff2ab8666f40fbf3f29d99d45ce
Christopher Groskopf <staringmonkey@gmail.com>
2012-01-11 16:40:56 -0600

    Revert "Embed source view in docs."
    This reverts commit efe4439e31cca70bcd63ebbb5cf167686beb4565.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 autodoc_member_order = 'bysource'
 
 intersphinx_mapping = {
