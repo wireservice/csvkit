bba4b9d07cce4195b20c89062aac566ba6f4144a
Christopher Groskopf <staringmonkey@gmail.com>
2012-02-20 10:52:34 -0600

    Rev to 0.4.3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='csvkit',
-    version='0.4.2',
+    version='0.4.3',
     description='A library of utilities for working with CSV, the king of tabular file formats.',
     long_description=open('README').read(),
     author='Christopher Groskopf',
@@ -48,5 +48,6 @@ setup(
         'argparse==1.2.1',
         'xlrd==0.7.1',
         'python-dateutil==1.5',
-        'sqlalchemy==0.6.6']
+        'sqlalchemy==0.6.6',
+        'openpyxl==1.5.7']
 )
