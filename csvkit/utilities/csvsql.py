1a2865428b1b5d6ce8733e6ccb39f7e2f2098d24
Christopher Groskopf <staringmonkey@gmail.com>
2012-02-29 19:03:52 -0600

    Implement --no-create csvsql option. Courtesy @soid. Closes #148.

--- csvkit/utilities/csvsql.py
+++ csvkit/utilities/csvsql.py
@@ -24,6 +24,8 @@ class CSVSQL(CSVKitUtility):
                             help='Specify a name for the table to be created. If omitted, the filename (minus extension) will be used.')
         self.argparser.add_argument('--no-constraints', dest='no_constraints', action='store_true',
                             help='Generate a schema without length limits or null checks. Useful when sampling big tables.')
+        self.argparser.add_argument('--no-create', dest='no_create', action='store_true',
+                            help='Skip creating a table. Only valid when --insert is specified.')
 
     def main(self):
         if self.args.table_name:
@@ -40,6 +42,9 @@ class CSVSQL(CSVKitUtility):
         if self.args.insert and not self.args.connection_string:
             self.argparser.error('The --insert option is only valid when --db is also specified.')
 
+        if self.args.no_create and not self.args.insert:
+            self.argparser.error('The --no-create option is only valid --insert is also specified.')
+
         csv_table = table.Table.from_csv(self.args.file, name=table_name, snifflimit=self.args.snifflimit, **self.reader_kwargs)
 
         # Direct connections to database
@@ -50,7 +55,9 @@ class CSVSQL(CSVKitUtility):
                 raise ImportError('You don\'t appear to have the necessary database backend installed for connection string you\'re trying to use.. Available backends include:\n\nPostgresql:\tpip install psycopg2\nMySQL:\t\tpip install MySQL-python\n\nFor details on connection strings and other backends, please see the SQLAlchemy documentation on dialects at: \n\nhttp://www.sqlalchemy.org/docs/dialects/\n\n')
 
             sql_table = sql.make_table(csv_table, table_name, self.args.no_constraints, metadata)
-            sql_table.create()
+
+            if not self.args.no_create:
+                sql_table.create()
 
             if self.args.insert:
                 insert = sql_table.insert()
