2234cbdc78e81329c4110f4eb4e69f429d9b6fb5
Christopher Groskopf <staringmonkey@gmail.com>
2012-07-23 20:47:28 -0500

    Fix for bug in latest dbf module. Pypy passes now.

--- csvkit/convert/dbase.py
+++ csvkit/convert/dbase.py
@@ -9,35 +9,33 @@ from csvkit import table
 def dbf2csv(f, **kwargs):
     """
     Convert a dBASE .dbf file to csv.
-
     """
-    db = dbf.Table(f.name)
+    with dbf.Table(f.name) as db:
+        headers = db.field_names
 
-    headers = db.field_names
+        column_ids = range(len(headers))
 
-    column_ids = range(len(headers))
+        data_columns = [[] for c in headers]
 
-    data_columns = [[] for c in headers]
+        for row in db:
+            for i, d in enumerate(row):
+                try:
+                    data_columns[i].append(unicode(row[column_ids[i]]).strip())
+                except IndexError:
+                    # Non-rectangular data is truncated
+                    break
 
-    for row in db:
-        for i, d in enumerate(row):
-            try:
-                data_columns[i].append(unicode(row[column_ids[i]]).strip())
-            except IndexError:
-                # Non-rectangular data is truncated
-                break
+        columns = []
 
-    columns = []
+        for i, c in enumerate(data_columns):
+            columns.append(table.Column(column_ids[i], headers[i], c))
 
-    for i, c in enumerate(data_columns):
-        columns.append(table.Column(column_ids[i], headers[i], c))
+        tab = table.Table(columns=columns) 
 
-    tab = table.Table(columns=columns) 
+        o = StringIO()
+        output = tab.to_csv(o)
+        output = o.getvalue()
+        o.close()
 
-    o = StringIO()
-    output = tab.to_csv(o)
-    output = o.getvalue()
-    o.close()
-
-    return output
+        return output
 
