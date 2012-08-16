dbed0b5b2f92c45ac8cbb1ed7a4db314b6e57fec
Christopher Groskopf <staringmonkey@gmail.com>
2012-08-16 11:36:09 -0500

    Scrub csvclean for code style.

--- csvkit/cleanup.py
+++ csvkit/cleanup.py
@@ -2,62 +2,79 @@
 
 from csvkit.exceptions import CSVTestException, LengthMismatchError
 
-def join_rows(rows,joiner=' '):
-    """Given a series of rows, return them as a single row where the inner edge cells are merged. By default joins with a single space character, but you can specify new-line, empty string, or anything else with the 'joiner' kwarg."""
+def join_rows(rows, joiner=' '):
+    """
+    Given a series of rows, return them as a single row where the inner edge cells are merged. By default joins with a single space character, but you can specify new-line, empty string, or anything else with the 'joiner' kwarg.
+    """
     rows = list(rows)
     fixed_row = rows[0][:]
+
     for row in rows[1:]:
         if len(row) == 0:
             row = ['']
-        fixed_row[-1] += "%s%s" % (joiner,row[0])
+        
+        fixed_row[-1] += "%s%s" % (joiner, row[0])
         fixed_row.extend(row[1:])
 
     return fixed_row
         
-def fix_length_errors(errs, target_line_length,joiner=' '):
-    """If possible, transform the rows backed up in the list of errors into rows of the correct length.
-       If the list of errors does not yet produce a row of target_line_length, return an empty array.
-       If the joining the list of error
+def fix_length_errors(errs, target_line_length, joiner=' '):
     """
-    if not errs: return []
+    If possible, transform the rows backed up in the list of errors into rows of the correct length.
+    If the list of errors does not yet produce a row of target_line_length, return an empty array.
+    """
+    if not errs:
+        return []
+
     fixed_rows = []
     backlog = []
+    
     for err in errs:
-        if type(err) is not LengthMismatchError: return [] # give up if any are not length errors
+        if type(err) is not LengthMismatchError:
+            return [] # give up if any are not length errors
+
         backlog.append(err)
         fixed_row = join_rows([err.row for err in backlog])
+
         if len(fixed_row) == target_line_length:
             fixed_rows.append(fixed_row)
             backlog = [] # reset
-        # elif
         
     return fixed_rows
 
 def extract_joinable_row_errors(errs):
     joinable = []
+
     for err in reversed(errs):
         if type(err) is not LengthMismatchError:
             break
+
         if joinable and err.line_number != joinable[-1].line_number - 1:
             break
+
         joinable.append(err)
 
-    joinable.reverse()    
+    joinable.reverse() 
+
     return joinable
 
 class RowChecker(object):
-    """When created with a reader, can yield clean rows from the reader and expose some information about what wasn't clean."""
+    """
+    Iterate over rows of a CSV producing cleaned rows and storing error rows.
+    """
     def __init__(self, reader):
-        super(RowChecker, self).__init__()
         self.reader = reader
         self.column_names = reader.next()
+
         self.input_rows = 1 
         self.errs = []
         self.rows_joined = 0
         self.joins = 0
-        
+
     def checked_rows(self):
-        """A generator which yields OK rows which are ready to write to output."""
+        """
+        A generator which yields rows which are ready to write to output.
+        """
         for row in self.reader:
             self.input_rows += 1 
             line_number = self.input_rows + 1 # add one for 1-based counting
@@ -65,25 +82,32 @@ class RowChecker(object):
             try:
                 if len(row) != len(self.column_names):
                     raise LengthMismatchError(line_number,row,len(self.column_names))
-                # any other tests?
+
                 yield row
             except LengthMismatchError, e:
                 self.errs.append(e)
-                # see if we can actually clean up those length mismatches
+
                 joinable_row_errors = extract_joinable_row_errors(self.errs)
+                
                 while joinable_row_errors:
                     fixed_row = join_rows([err.row for err in joinable_row_errors], joiner=' ')
-                    if len(fixed_row) < len(self.column_names): break
+
+                    if len(fixed_row) < len(self.column_names):
+                        break
+
                     if len(fixed_row) == len(self.column_names):
                         self.rows_joined += len(joinable_row_errors)
                         self.joins += 1
+
                         yield fixed_row
+                        
                         for fixed in joinable_row_errors:
                             self.errs.remove(fixed)
+                        
                         break
+
                     joinable_row_errors = joinable_row_errors[1:] # keep trying in case we're too long because of a straggler
 
             except CSVTestException, e:
                 self.errs.append(e)
-        
         
--- csvkit/utilities/csvclean.py
+++ csvkit/utilities/csvclean.py
@@ -7,7 +7,7 @@ from csvkit.cli import CSVKitUtility
 from csvkit.cleanup import RowChecker
 
 class CSVClean(CSVKitUtility):
-    description = 'Fix errors in a CSV file.'
+    description = 'Fix common errors in a CSV file.'
 
     def add_arguments(self):
         self.argparser.add_argument('-n', '--dry-run', dest='dryrun', action='store_true',
@@ -18,35 +18,41 @@ class CSVClean(CSVKitUtility):
 
         if self.args.dryrun:
             checker = RowChecker(reader)
+
             for row in checker.checked_rows():
                 pass
+            
             if checker.errs:
                 for e in checker.errs:
                     self.output_file.write('Line %i: %s\n' % (e.line_number,e.msg))
             else:
                 self.output_file.write('No errors.\n')
+            
             if checker.joins:
                 self.output_file.write('%i rows would have been joined/reduced to %i rows after eliminating expected internal line breaks.\n' % (checker.rows_joined, checker.joins))
         else:
             base,ext = splitext(self.args.file.name)
-            # should we preserve delimiters and other dialect args from CLI?
+
             cleaned_file = CSVKitWriter(open('%s_out.csv' % base,'w'), **self.writer_kwargs)
 
             checker = RowChecker(reader)
             cleaned_file.writerow(checker.column_names)
+
             for row in checker.checked_rows():
                 cleaned_file.writerow(row)
             
             if checker.errs:
-                # should we preserve delimiters and other dialect args from CLI?
                 err_filename = '%s_err.csv' % base
                 err_file = CSVKitWriter(open(err_filename, 'w'), **self.writer_kwargs)
-                err_header = ['line_number','msg']
+
+                err_header = ['line_number', 'msg']
                 err_header.extend(checker.column_names)
                 err_file.writerow(err_header)
+
                 for e in checker.errs:
                     err_file.writerow(self._format_error_row(e))
                     err_count = len(checker.errs)
+
                 self.output_file.write('%i error%s logged to %s\n' % (err_count,'' if err_count == 1 else 's', err_filename))
             else:
                 self.output_file.write('No errors.\n')
@@ -57,6 +63,7 @@ class CSVClean(CSVKitUtility):
     def _format_error_row(self, e):
         err_row = [e.line_number, e.msg]
         err_row.extend(e.row)
+
         return err_row
 
 def launch_new_instance():
