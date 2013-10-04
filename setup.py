c196032ded3bdd72a8d62d95629a762bf41ad42b
Jeff Johnson <jeffcjohnson@gmail.com>
2013-10-04 09:22:36 -0400

    add csves to setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
             'csvsort = csvkit.utilities.csvsort:launch_new_instance',
             'csvgrep = csvkit.utilities.csvgrep:launch_new_instance',
             'csvjson = csvkit.utilities.csvjson:launch_new_instance',
+            'csves = csvkit.utilities.csves:launch_new_instance',
             'csvpy = csvkit.utilities.csvpy:launch_new_instance'
         ]
     },
