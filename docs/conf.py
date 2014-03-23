0091d608785e326170e06540717eb8ce5c7be0f3
Christopher Groskopf <staringmonkey@gmail.com>
2014-03-23 13:42:14 -0700

    Update copyright date.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'csvkit'
-copyright = u'2012, Christopher Groskopf'
+copyright = u'2014, Christopher Groskopf'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
