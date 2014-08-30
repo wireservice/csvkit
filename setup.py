af9112b0340f6a39d438fae569ad3ec7039c0407
Christopher Groskopf <staringmonkey@gmail.com>
2014-08-30 09:47:57 -0500

    Add docs for csvformat.

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'console_scripts': [
             'csvclean = csvkit.utilities.csvclean:launch_new_instance',
             'csvcut = csvkit.utilities.csvcut:launch_new_instance',
+            'csvformat = csvkit.utilities.csvformat:launch_new_instance',
             'csvgrep = csvkit.utilities.csvgrep:launch_new_instance',
             'csvjoin = csvkit.utilities.csvjoin:launch_new_instance',
             'csvjson = csvkit.utilities.csvjson:launch_new_instance',
