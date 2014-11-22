429c5a3df3a18a83679db2826197b357edd00d9a
Christopher Groskopf <staringmonkey@gmail.com>
2014-11-22 12:20:30 -0700

    Fix tests for csvlook and csvstat. Closes #346. Closes #363. (Again.)

--- tests/test_utilities/test_csvstat.py
+++ tests/test_utilities/test_csvstat.py
@@ -12,38 +12,38 @@ from csvkit.utilities.csvstat import CSVStat
 class TestCSVStat(unittest.TestCase):
     def test_runs(self):
         args = ['examples/test_utf8.csv']
-        output_file = six.BytesIO()
+        output_file = six.StringIO()
 
         utility = CSVStat(args, output_file)
         utility.main()
 
     def test_encoding(self):
         args = ['-e', 'latin1', 'examples/test_latin1.csv']
-        output_file = six.BytesIO()
+        output_file = six.StringIO()
 
         utility = CSVStat(args, output_file)
         utility.main()
 
     def test_no_header_row(self):
         args = ['-H', '-c', '2', 'examples/no_header_row.csv']
-        output_file = six.BytesIO()
-
-        utility = CSVStat(args, output_file)
-        utility.main()
-
-        stats = output_file.getvalue().decode('utf-8')
-
-        self.assertFalse('column1' in stats)
-        self.assertTrue('column2' in stats)
-
-    def test_count_only(self):
-        args = ['--count', 'examples/dummy.csv']
-        output_file = six.BytesIO()
+        output_file = six.StringIO()
 
         utility = CSVStat(args, output_file)
         utility.main()
 
         stats = output_file.getvalue()
 
-        self.assertEqual(stats, b'Row count: 1\n')
+        self.assertFalse('column1' in stats)
+        self.assertTrue('column2' in stats)
+
+    def test_count_only(self):
+        args = ['--count', 'examples/dummy.csv']
+        output_file = six.StringIO()
+
+        utility = CSVStat(args, output_file)
+        utility.main()
+
+        stats = output_file.getvalue()
+
+        self.assertEqual(stats, 'Row count: 1\n')
 
