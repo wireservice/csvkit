99901c2095f31f4eaf01753dcc1ff6ed8b60b5c3
Christopher Groskopf <staringmonkey@gmail.com>
2015-10-30 17:30:35 -0500

    Convert convert.dbase to use agate.

--- tests/test_convert/test_dbf.py
+++ tests/test_convert/test_dbf.py
@@ -7,15 +7,14 @@ except ImportError:
 
 import six
 
-if six.PY2:
-    from csvkit.convert import dbase
+from csvkit.convert import dbase
 
-@unittest.skipUnless(six.PY2, "Only supported in Python 2.")
+# @unittest.skipUnless(six.PY2, "Only supported in Python 2.")
 class TestDBF(unittest.TestCase):
     def test_dbf(self):
         with open('examples/testdbf.dbf', 'rb') as f:
             output = dbase.dbf2csv(f)
 
         with open('examples/testdbf_converted.csv', 'r') as f:
+            print (output)
             self.assertEquals(f.read(), output)
-
