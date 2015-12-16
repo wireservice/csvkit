a4d162f9c64fcbc4c5bca43c1b196df43470be5e
ayushNIT007 <ayush@zopper.com>
2015-12-16 17:49:00 +0530

    Modifying writerows function to catch Unicode Encode Error

--- csvkit/py2.py
+++ csvkit/py2.py
@@ -86,10 +86,23 @@ class CSVKitDictWriter(unicsv.UnicodeCSVDictWriter):
 
         unicsv.UnicodeCSVDictWriter.writerow(self, row)
 
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
+    # Modifying writerows to catch UnicodeEncodeError
+    def writerows(self, rowdicts):
+        # Iterating every row one by one
+        for rowdict in rowdicts:
+            # Accessing key value pair in a rowdict
+            for key, value in rowdict.items():
+                # Including the value in try block
+                try:
+                    # Decoding the utf-8 into target ascii with ignore parameter
+                    value.decode('ascii').encode('ascii', 'ignore')
+                except UnicodeEncodeError:
+                    # Replacing the string with empty value if any Encoding error occurs
+                    rowdict[key] = ''
+                except AttributeError:
+                    continue
 
+            writer.writerow(rowdict)
 def reader(*args, **kwargs):
     """
     A drop-in replacement for Python's :func:`csv.reader` that leverages :class:`csvkit.py2.CSVKitReader`.
