1450d9a58f2fc054c6c283fd06b8d5882bddab72
Deps <deps81@gmail.com>
2016-01-07 19:23:04 +0100

    csvlook now use plus signs in the corners.

--- csvkit/utilities/csvlook.py
+++ csvkit/utilities/csvlook.py
@@ -54,7 +54,7 @@ class CSVLook(CSVKitUtility):
 
         # Dashes span each width with '+' character at intersection of
         # horizontal and vertical dividers.
-        divider = '|--' + '-+-'.join('-'* w for w in widths) + '--|'
+        divider = '+--' + '-+-'.join('-'* w for w in widths) + '--+'
 
         self.output_file.write('%s\n' % divider)
 
