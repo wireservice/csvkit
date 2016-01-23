809a4856aea0968ba9109257a1401aadc6794edf
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-01-23 10:42:11 -0600

    Fix broken xls tests.

--- csvkit/convert/xls.py
+++ csvkit/convert/xls.py
@@ -10,7 +10,7 @@ def xls2csv(f, **kwargs):
     """
     Convert an Excel .xls file to csv.
     """
-    table = agate.Table.from_xls(f, **kwargs)
+    table = agate.Table.from_xls(f, sheet=kwargs.get('sheet', None))
 
     o = six.StringIO()
     output = table.to_csv(o)
--- csvkit/convert/xlsx.py
+++ csvkit/convert/xlsx.py
@@ -10,7 +10,7 @@ def xlsx2csv(f, **kwargs):
     """
     Convert an Excel .xlsx file to csv.
     """
-    table = agate.Table.from_xlsx(f, **kwargs)
+    table = agate.Table.from_xlsx(f, sheet=kwargs.get('sheet', None))
 
     o = six.StringIO()
     output = table.to_csv(o)
