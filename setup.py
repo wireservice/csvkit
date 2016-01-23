a37cd8434ca96c3328b705749eadb28079d579db
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-01-23 10:59:17 -0600

    Fix py2 tox reqs.

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
