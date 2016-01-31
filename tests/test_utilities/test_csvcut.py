99acb1e2cdc98ce0230d7667cb6be68a7967da4e
James McKinney <james@slashpoundbang.com>
2016-01-31 14:37:43 -0500

    Reduce duplication in test_csvcut.py

--- tests/test_utilities/test_csvcut.py
+++ tests/test_utilities/test_csvcut.py
@@ -20,98 +20,61 @@ from tests import ColumnsTests, NamesTests
 class TestCSVCut(unittest.TestCase, ColumnsTests, NamesTests):
     Utility = CSVCut
 
+    def assertRows(self, args, rows):
+        output_file = six.StringIO()
+        utility = CSVCut(args, output_file)
+
+        utility.main()
+
+        input_file = six.StringIO(output_file.getvalue())
+        reader = agate.reader(input_file)
+
+        for row in rows:
+            self.assertEqual(next(reader), row)
+
     def test_launch_new_instance(self):
         with patch.object(sys, 'argv', ['csvcut', 'examples/dummy.csv']):
             launch_new_instance()
 
     def test_simple(self):
-        args = ['-c', '1,3', 'examples/dummy.csv']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['a', 'c'])
-        self.assertEqual(next(reader), ['1', '3'])
+        self.assertRows(['-c', '1,3', 'examples/dummy.csv'], [
+            ['a', 'c'],
+            ['1', '3'],
+        ])
 
     def test_unicode(self):
-        args = ['-c', '1,3', 'examples/test_utf8.csv']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['foo', 'baz'])
-        self.assertEqual(next(reader), ['1', '3'])
-        self.assertEqual(next(reader), ['4', u'ʤ'])
+        self.assertRows(['-c', '1,3', 'examples/test_utf8.csv'], [
+            ['foo', 'baz'],
+            ['1', '3'],
+            ['4', u'ʤ'],
+        ])
 
     def test_with_gzip(self):
-        args = ['-c', '1,3', 'examples/dummy.csv.gz']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['a', 'c'])
-        self.assertEqual(next(reader), ['1', '3'])
+        self.assertRows(['-c', '1,3', 'examples/dummy.csv.gz'], [
+            ['a', 'c'],
+            ['1', '3'],
+        ])
 
     def test_with_bzip2(self):
-        args = ['-c', '1,3', 'examples/dummy.csv.bz2']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['a', 'c'])
-        self.assertEqual(next(reader), ['1', '3'])
+        self.assertRows(['-c', '1,3', 'examples/dummy.csv.bz2'], [
+            ['a', 'c'],
+            ['1', '3'],
+        ])
 
     def test_exclude(self):
-        args = ['-C', '1,3', 'examples/dummy.csv']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['b'])
-        self.assertEqual(next(reader), ['2'])
+        self.assertRows(['-C', '1,3', 'examples/dummy.csv'], [
+            ['b'],
+            ['2'],
+        ])
 
     def test_include_and_exclude(self):
-        args = ['-c', '1,3', '-C', '3', 'examples/dummy.csv']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['a'])
-        self.assertEqual(next(reader), ['1'])
+        self.assertRows(['-c', '1,3', '-C', '3', 'examples/dummy.csv'], [
+            ['a'],
+            ['1'],
+        ])
 
     def test_no_header_row(self):
-        args = ['-c', '2', '--no-header-row', 'examples/no_header_row.csv']
-        output_file = six.StringIO()
-        utility = CSVCut(args, output_file)
-
-        utility.main()
-
-        input_file = six.StringIO(output_file.getvalue())
-        reader = agate.reader(input_file)
-
-        self.assertEqual(next(reader), ['B'])
-        self.assertEqual(next(reader), ['2'])
+        self.assertRows(['-c', '2', '--no-header-row', 'examples/no_header_row.csv'], [
+            ['B'],
+            ['2'],
+        ])
