e863dbd5de4b591385499d3242fd4bcb6517a065
James McKinney <james@slashpoundbang.com>
2016-02-04 00:15:10 -0500

    csvlook: Add support for --snifflimit and --no-inference

--- csvkit/utilities/csvlook.py
+++ csvkit/utilities/csvlook.py
@@ -9,10 +9,19 @@ class CSVLook(CSVKitUtility):
     description = 'Render a CSV file in the console as a fixed-width table.'
 
     def add_arguments(self):
-        pass
+        self.argparser.add_argument('-y', '--snifflimit', dest='sniff_limit', type=int,
+                                    help='Limit CSV dialect sniffing to the specified number of bytes. Specify "0" to disable sniffing entirely.')
+        self.argparser.add_argument('--no-inference', dest='no_inference', action='store_true',
+                                    help='Disable type inference when parsing the input.')
 
     def main(self):
-        agate.Table.from_csv(self.input_file, header=not self.args.no_header_row, line_numbers=self.args.line_numbers, **self.reader_kwargs).print_table(output=self.output_file)
+        if self.args.no_inference:
+            column_types = agate.TypeTester(limit=0)
+        else:
+            column_types = None
+
+        table = agate.Table.from_csv(self.input_file, sniff_limit=self.args.sniff_limit, header=not self.args.no_header_row, column_types=column_types, line_numbers=self.args.line_numbers, **self.reader_kwargs)
+        table.print_table(output=self.output_file)
 
 
 def launch_new_instance():
