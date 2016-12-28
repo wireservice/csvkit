6a78eef43311410b4d91fcde1bd29bdd4d429661
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-12-28 09:37:29 -0600

    Clean up Py2.7 deprecation warnings.

--- tests/test_utilities/test_csvjson.py
+++ tests/test_utilities/test_csvjson.py
@@ -38,7 +38,7 @@ class TestCSVJSON(CSVKitTestCase, EmptyFileTests):
         output = self.get_output(['-i', '4', 'examples/dummy.csv'])
         js = json.loads(output)
         self.assertDictEqual(js[0], {'a': True, 'c': 3.0, 'b': 2.0})
-        six.assertRegex(self, output, '        "a": true,')
+        self.assertRegex(output, '        "a": true,')
 
     def test_keying(self):
         js = json.loads(self.get_output(['-k', 'a', 'examples/dummy.csv']))
@@ -47,7 +47,7 @@ class TestCSVJSON(CSVKitTestCase, EmptyFileTests):
     def test_duplicate_keys(self):
         output_file = six.StringIO()
         utility = CSVJSON(['-k', 'a', 'examples/dummy3.csv'], output_file)
-        six.assertRaisesRegex(self, ValueError, 'Value True is not unique in the key column.', utility.run)
+        self.assertRaisesRegex(ValueError, 'Value True is not unique in the key column.', utility.run)
         output_file.close()
 
     def test_geojson(self):
