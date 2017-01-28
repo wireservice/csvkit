97ae6523a90c03bb682e0ae64257746f68f6a578
James McKinney <james@slashpoundbang.com>
2017-01-27 19:22:36 -0500

    Add default value for skip_lines

--- csvkit/cli.py
+++ csvkit/cli.py
@@ -167,7 +167,7 @@ class CSVKitUtility(object):
             self.argparser.add_argument('-H', '--no-header-row', dest='no_header_row', action='store_true',
                                         help='Specify that the input CSV file has no header row. Will create default headers (a,b,c,...).')
         if 'L' not in self.override_flags:
-            self.argparser.add_argument('-L', '--skip-lines', dest='skip_lines', type=int,
+            self.argparser.add_argument('-L', '--skip-lines', dest='skip_lines', type=int, default=0,
                                         help='Specify the number of initial lines to skip (e.g. comments, copyright notices, empty rows).')
         if 'v' not in self.override_flags:
             self.argparser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
