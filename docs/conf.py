da9c04c1cd882d013e79575cc4c5f7de18f6babd
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-06-13 18:59:11 -0400

    chore: flake8

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
